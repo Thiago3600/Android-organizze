5e6e3b4895807431dbe02ef69df337c818f2a092
thiag <thiagos3600@gmail.com>
2020-12-28 11:27:02 -0300

    nova alteração de  comentario

---
+++ app/src/main/java/com/example/organizze/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.organizze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //Comentario ouabnosdb
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
